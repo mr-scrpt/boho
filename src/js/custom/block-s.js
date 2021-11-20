475e8008b9449758103b06451bd25efcc53f47c5
mr-scrpt <mr.scrpt@gmail.com>
2021-11-20 16:51:33 +0200

    refactoring: menu mobile and main menu

--- src/js/custom/block-s.js
+++ src/js/custom/block-s.js
@@ -1,113 +1,24 @@
 $(document).ready(() => {
 	/* Настройки */
-	const mainClass = `block-s`;
+	const mobileMenu = $(".page__menu-mobile");
+	const controllerMenu = $(".menu-main__controller");
+	const closeMenu = $(".menu-mobile__closebox");
+	const page = $(".page");
 
-	/* Настройки КОНЕЦ*/
+	controllerMenu.on("click", () => {
+		if (!mobileMenu.hasClass("page__menu-mobile_open")) {
+			mobileMenu.addClass("page__menu-mobile_open");
+			mobileMenu.removeClass("page__menu-mobile_close");
+			page.addClass("page_menu_open");
+		}
+	});
 
-	const $menuCollection = $(`.${mainClass}`);
-	$menuCollection.each((idx, item) => {
-		const $menu = $(item);
-		let mobileSize = $(item).attr("data-adaptive-width");
-		const $inner = $menu.children(`.${mainClass}__inner`);
-		const $body = $inner.children(`.${mainClass}__body`);
-		const $controller = $menu.find(`.${mainClass}__controller`);
-		const $closer = $menu.find(`.${mainClass}__close`);
-		const $overflowBlock = $(".layout");
-		clearClassAndStyle($menu, $body, mainClass, mobileSize);
-		controllMenu($controller, mainClass, mobileSize, $overflowBlock);
-		controllMenu($closer, mainClass, mobileSize, $overflowBlock);
-		closeOutsideMenu($body, mainClass);
-		adaptiveMode(mobileSize, $body);
-		/*offsetBody($inner, $body, mobileSize);*/
-
-		$(window).resize(() => {
-			mobileSize = $(item).attr("data-adaptive-width");
-			clearClassAndStyle($menu, $body, mainClass, mobileSize);
-			adaptiveMode(mobileSize, $body);
-			/*offsetBody($inner, $body, mobileSize);*/
-			controllMenu($controller, mainClass);
-		});
+	closeMenu.on("click", () => {
+		if (mobileMenu.hasClass("page__menu-mobile_open")) {
+			mobileMenu.removeClass("page__menu-mobile_open");
+			mobileMenu.addClass("page__menu-mobile_close");
+			page.removeClass("page_menu_open");
+			page.addClass("page_menu_close");
+		}
 	});
 });
-
-const checkMobileMode = (mobileSize) => {
-	let width = getSize();
-	if (width < mobileSize) {
-		return true;
-	}
-	return false;
-};
-
-const offsetBody = ($inner, $body, mobileSize) => {
-	const mode = checkMobileMode(mobileSize);
-	let width = getSize();
-	if (mode) {
-		const height = $inner.outerHeight();
-		$body.css({ top: height });
-		console.log("-> set height", width);
-	} else {
-		$body.css({ top: "auto" });
-		console.log("-> unset height", width);
-	}
-};
-
-const adaptiveMode = (mobileSize, $body) => {
-	const mode = checkMobileMode(mobileSize);
-	if (!mode) {
-		$body.css({ position: "relative", transform: "translateX(0)" });
-	} else {
-		$body.removeAttr("style");
-	}
-};
-
-const controllMenu = ($controller, mainClass, mobileSize, $overflowBlock) => {
-	$controller.on("click", (e) => {
-		e.stopPropagation();
-
-		const mode = checkMobileMode(mobileSize);
-
-		if (mode) {
-			const $currentMenu = $(e.target).closest(`.${mainClass}`);
-
-			$currentMenu.hasClass(`${mainClass}_open`)
-				? $currentMenu.removeClass(`${mainClass}_open`) &&
-				  $currentMenu.addClass(`${mainClass}_close`) &&
-				  $overflowBlock.removeClass("layout_menu")
-				: $currentMenu.removeClass(`${mainClass}_close`) &&
-				  $currentMenu.addClass(`${mainClass}_open`) &&
-				  $overflowBlock.addClass("layout_menu");
-		}
-	});
-};
-
-const closeOutsideMenu = ($body, mainClass) => {
-	$body.on("click", (e) => {
-		e.stopPropagation();
-	});
-
-	$("html").click(function () {
-		const $openMenu = $(`.${mainClass}_open`);
-		$openMenu.each(function (_, item) {
-			$(item).hasClass(`${mainClass}_open`) &&
-				$(item).removeClass(`${mainClass}_open`) &&
-				$(item).addClass(`${mainClass}_close`);
-		});
-	});
-};
-
-const clearClassAndStyle = ($menu, $body, mainClass, mobileSize) => {
-	let width = getSize();
-	if (width >= mobileSize) {
-		if ($menu.hasClass(`${mainClass}_close`)) {
-			$menu.removeClass(`${mainClass}_close`);
-		}
-
-		if ($menu.hasClass(`${mainClass}_open`)) {
-			$menu.removeClass(`${mainClass}_open`);
-		}
-
-		$body.removeAttr("style");
-	}
-};
-
-const getSize = () => $(window).width();
