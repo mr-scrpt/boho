1a7d9d6ba616680b645c389ff000b543b77a8663
mr-scrpt <mr.scrpt@gmail.com>
2021-11-23 11:19:05 +0200

    implemented notice block

--- src/js/custom/notice.js
+++ src/js/custom/notice.js
@@ -1,10 +1,16 @@
 $(document).ready(() => {
 	const $button = $(".markets__read-more-button");
 	const $block = $(".page__notice");
+	const $closer = $(".notice__close");
 	console.log("-> 11");
 
 	$button.on("click", (e) => {
 		e.preventDefault();
 		$block.addClass("page__notice_open");
+	});
+
+	$closer.on("click", (e) => {
+		$block.removeClass("page__notice_open");
+		$block.addClass("page__notice_close");
 	});
 });
