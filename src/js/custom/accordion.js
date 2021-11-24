34f3c3bc3c7798ebdcbaf571c58869f7c44ee691
mr-scrpt <mr.scrpt@gmail.com>
2021-11-24 14:01:45 +0200

    implemented: qa-mob section

---
+++ src/js/custom/accordion.js
@@ -0,0 +1,15 @@
+$(function () {
+	$(".qa-mob__item").accordion({
+		active: false,
+		heightStyle: "content",
+		collapsible: true,
+		classes: {
+			"ui-accordion-header-active": "qa-mob__item-titlebox_active",
+		},
+	});
+});
+
+/*$(function () {
+	$("#accordion").accordion();
+});
+*/
